c9d690d171616ad10ec671f769cbc89dcb5df848
yanghuiwen <cainiaodj@qq.com>
2020-02-23 20:03:05 +0800

    快排算法

--- studyCode/quickSort/quickSort.go
+++ studyCode/quickSort/quickSort.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+	"time"
+)
 
 func quickSortCustom(nums *[]int, start int, end int) {
 	// 起点和终点重合的时候，退出
@@ -59,49 +64,49 @@ func (s IntSlice) Len() int           { return len(s) }
 func (s IntSlice) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
 
 func main() {
-	//startTime := time.Now()
-	//for i := 1000000; i <= 10000000; i += 1000000 {
-	//    numsIn := make(IntSlice, 0)
-	//    numsMy := make([]int, 0)
-	//    for n := 0; n < i; n++ {
-	//        //rand.Seed(int64(n))
-	//        x := rand.Intn(10000)
-	//        numsIn = append(numsIn, x)
-	//        numsMy = append(numsMy, x)
-	//
-	//    }
-	//
-	//    l := len(numsMy)
-	//    //fmt.Println("原始:", numsMy, "排序个数：", l)
-	//    fmt.Println("排序个数：", l)
-	//
-	//    // ---------------- 内置排序 开始----------------
-	//    startTime = time.Now()
-	//    sort.Ints(numsIn)
-	//    cost := time.Since(startTime)
-	//    fmt.Println("内置排序耗时：", cost)
-	//    // ---------------- 内置排序 结束----------------
-	//
-	//    // ---------------- 自研排序 开始----------------
-	//    startTime = time.Now()
-	//    quickSortCustom(&numsMy, 0, l-1)
-	//    cost = time.Since(startTime)
-	//    fmt.Println("自研排序耗时：", cost)
-	//    // ---------------- 自研排序 结束----------------
-	//
-	//    fmt.Println("结果抽查:", numsMy[l/2] == numsIn[l/2])
-	//
-	//    //fmt.Println("内置:", numsIn)
-	//    //fmt.Println("自研:", numsMy)
-	//
-	//    fmt.Println(" ")
-	//
-	//}
+	startTime := time.Now()
+	for i := 100000; i <= 1000000; i += 100000 {
+		numsIn := make(IntSlice, 0)
+		numsMy := make([]int, 0)
+		for n := 0; n < i; n++ {
+			//rand.Seed(int64(n))
+			x := rand.Intn(10000)
+			numsIn = append(numsIn, x)
+			numsMy = append(numsMy, x)
 
-	nums := []int{8081, 7887, 1847, 4059, 2081, 1318, 4425, 2540, 456, 3300}
-	fmt.Println("原始的数据:", nums)
-	quickSortCustom(&nums, 0, len(nums)-1)
-	fmt.Println("自己排序后:", nums)
+		}
+
+		l := len(numsMy)
+		//fmt.Println("原始:", numsMy, "排序个数：", l)
+		fmt.Println("排序个数：", l)
+
+		// ---------------- 内置排序 开始----------------
+		startTime = time.Now()
+		sort.Ints(numsIn)
+		cost := time.Since(startTime)
+		fmt.Println("内置排序耗时：", cost)
+		// ---------------- 内置排序 结束----------------
+
+		// ---------------- 手写排序 开始----------------
+		startTime = time.Now()
+		quickSortCustom(&numsMy, 0, l-1)
+		cost = time.Since(startTime)
+		fmt.Println("手写排序耗时：", cost)
+		// ---------------- 手写排序 结束----------------
+
+		fmt.Println("结果抽查:", numsMy[l/2] == numsIn[l/2])
+
+		//fmt.Println("内置:", numsIn)
+		//fmt.Println("手写:", numsMy)
+
+		fmt.Println(" ")
+
+	}
+
+	//nums := []int{8081, 7887, 1847, 4059, 2081, 1318, 4425, 2540, 456, 3300}
+	//fmt.Println("原始的数据:", nums)
+	//quickSortCustom(&nums, 0, len(nums)-1)
+	//fmt.Println("自己排序后:", nums)
 	//
 	//nums2 := IntSlice{8081, 7887, 1847, 4059, 2081, 1318, 4425, 2540, 456, 3300}
 	////fmt.Println("排序前:",nums2)
